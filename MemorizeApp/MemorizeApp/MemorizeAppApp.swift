2b9c9840075d86c2bf325eb7642d952598b4a109
KarVarr <megatr9n@gmail.com>
2023-02-06 00:28:20 +0300

    first commit

---
+++ MemorizeApp/MemorizeApp/MemorizeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeAppApp.swift
+//  MemorizeApp
+//
+//  Created by Karen Vardanian on 06.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
