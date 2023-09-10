1738733cf335a7496c8fec35db2a2b5de4dd5cbb
KarVarr <megatr9n@gmail.com>
2023-09-10 15:46:44 +0400

    added tableView and array hardcode

---
+++ Todoey/Todoey/TodoListViewController.swift
@@ -0,0 +1,32 @@
+//
+//  TodoListViewController.swift
+//  Todoey
+//
+//  Created by Karen Vardanian on 10.09.2023.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+    
+    let itemArray = ["Find Mike", "Buy Eggos", "Destory Demogorgon"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        itemArray.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell",for: indexPath)
+        
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+}
+
