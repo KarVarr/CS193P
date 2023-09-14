c8833478dd19eecaadfc92bce7310a2f18869a88
KarVarr <megatr9n@gmail.com>
2023-09-14 16:14:59 +0400

    fix some bug

--- Todoey/Todoey/Controller/CategoryTableViewController.swift
+++ Todoey/Todoey/Controller/CategoryTableViewController.swift
@@ -42,6 +42,18 @@ class CategoryTableViewController: UITableViewController {
                 return cell
     }
     
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "goToItems", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destinationVC = segue.destination as! TodoListViewController
+        
+        if let indexPath = tableView.indexPathForSelectedRow {
+            destinationVC.selectedCategory = categories[indexPath.row]
+        }
+    }
+    
 
     func saveItem() {
         do {
